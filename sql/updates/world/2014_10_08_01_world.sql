ed926acac248d1c34fa11c4097cdab1cab2243fe
Edmundo Freitas <edmundo.m.o.f@gmail.com>
2014-10-08 14:53:47 +0100

    Added npc_shard_of_torment

---
+++ sql/updates/world/2014_10_08_01_world.sql
@@ -0,0 +1,3 @@
+--Added npc_shard_of_tornment to creature_template
+UPDATE `creature_template` SET `ScriptName`='npc_shard_of_torment' WHERE entry='53495';
+UPDATE `creature_template` SET `unit_flags`=2 WHERE entry='53495';
